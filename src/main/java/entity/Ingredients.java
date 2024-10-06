4b346ca2177671c160d50e299d2203eb2e4ef0c6
Karissa Halverson <kdhalverson@madisoncollege.edu>
2024-10-06 14:16:14 -0500

    Filled in the Ingredients and Recipe classes with getters, setters, and overwrote some methods

--- src/main/java/entity/Ingredients.java
+++ src/main/java/entity/Ingredients.java
@@ -1,4 +1,88 @@
 package entity;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.Objects;
+
+/**
+ * This class will hold information about ingredients and its data.
+ *
+ * @author Karissa
+ */
 public class Ingredients {
+    private int ingredientID;
+    private String ingredientName;
+
+
+    /**
+     * Instantiates a new Ingredient.
+     */
+    public Ingredients() {
+
+    }
+
+    /**
+     * Instantiates a new Ingredient with the Ingredients name.
+     * @param ingredientName the name of the ingredient
+     */
+    public Ingredients(String ingredientName) {
+        this.ingredientName = ingredientName;
+    }
+
+
+    /**
+     * Gets the ingredients ID number.
+     * @return the ingredients ID number
+     */
+    public int getIngredientID() {
+        return ingredientID;
+    }
+
+    /**
+     * Sets the ingredients ID number.
+     * @param ingredientID the ingredients ID number
+     */
+    public void setIngredientID(int ingredientID) {
+        this.ingredientID = ingredientID;
+    }
+
+
+    /**
+     * Get the ingredients name.
+     * @return the name of the ingredient
+     */
+    public String getIngredientName() {
+        return ingredientName;
+    }
+
+    /**
+     * Sets the ingredients name.
+     * @param ingredientName the name of the ingredient
+     */
+    public void setIngredientName(String ingredientName) {
+        this.ingredientName = ingredientName;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ingredients that = (Ingredients) o;
+        return ingredientID == that.ingredientID && Objects.equals(ingredientName, that.ingredientName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ingredientID, ingredientName);
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredients{" +
+                "ingredientID=" + ingredientID +
+                ", ingredientName='" + ingredientName + '\'' +
+                '}';
+    }
 }
--- src/main/java/entity/Recipes.java
+++ src/main/java/entity/Recipes.java
@@ -1,4 +1,90 @@
 package entity;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.Objects;
+
+/**
+ * This class will hold information about a recipe and be able to access
+ * data in that recipe.
+ *
+ * @author Karissa
+ */
 public class Recipes {
+    private int recipeID;
+    private String recipeName;
+
+
+    /**
+     * Instantiates a new Recipe name.
+     */
+    public Recipes() {
+
+    }
+
+    /**
+     * Instantiates a new Recipe with the recipe name.
+     *
+     * @param recipeName the recipes name
+     */
+    public Recipes(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+
+    /**
+     * Get the recipe ID number.
+     * @return the recipe ID number
+     */
+    public int getRecipeID() {
+        return recipeID;
+    }
+
+    /**
+     * Sets the recipe ID number.
+     * @param recipeID the recipe ID number
+     */
+    public void setRecipeID(int recipeID) {
+        this.recipeID = recipeID;
+    }
+
+
+    /**
+     * Gets the recipes name.
+     * @return the recipes name
+     */
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    /**
+     * Sets the recipes name.
+     * @param recipeName the recipes name
+     */
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Recipes recipes = (Recipes) o;
+        return recipeID == recipes.recipeID && Objects.equals(recipeName, recipes.recipeName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(recipeID, recipeName);
+    }
+
+    @Override
+    public String toString() {
+        return "Recipes{" +
+                "recipeID=" + recipeID +
+                ", recipeName='" + recipeName + '\'' +
+                '}';
+    }
 }
