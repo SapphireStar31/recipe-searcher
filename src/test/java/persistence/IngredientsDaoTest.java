a9ad21284ee4ab66af1d1efdd3097b5014b69c5f
Karissa Halverson <kdhalverson@madisoncollege.edu>
2024-10-26 20:57:58 -0500

    Added more tests for Ingredients DAO

--- src/test/java/persistence/IngredientsDaoTest.java
+++ src/test/java/persistence/IngredientsDaoTest.java
@@ -4,6 +4,9 @@ import entity.Ingredients;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import utilities.Database;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class IngredientsDaoTest {
@@ -18,26 +21,46 @@ class IngredientsDaoTest {
 
     @Test
     void getAll() {
+        List<Ingredients> allIngredients = ingredientDao.getAll();
+        assertEquals(17, allIngredients.size());
     }
 
     @Test
     void getById() {
+        Ingredients retrievedIngredient = (Ingredients)ingredientDao.getById(3);
+        assertNotNull(retrievedIngredient);
+        assertEquals(ingredientDao.getById(3), retrievedIngredient);
     }
 
     @Test
     void delete() {
+        ingredientDao.delete(ingredientDao.getById(4));
+        assertNull(ingredientDao.getById(4));
     }
 
     @Test
     void insert() {
+        Ingredients newIngredient = new Ingredients("Potato");
+        int insertedIngredientID = ingredientDao.insert(newIngredient);
+        Ingredients insertedIngredient = (Ingredients)ingredientDao.getById(insertedIngredientID);
+        assertNotEquals(0, insertedIngredientID);
+        assertEquals("Potato", insertedIngredient.getIngredientName());
     }
 
     @Test
     void update() {
+        Ingredients ingredientToUpdate = (Ingredients)ingredientDao.getById(2);
+        ingredientToUpdate.setIngredientName("Cage-free Eggs");
+        ingredientDao.update(ingredientToUpdate);
+        Ingredients updatedIngredient = (Ingredients)ingredientDao.getById(2);
+        assertEquals("Cage-free Eggs", updatedIngredient.getIngredientName());
     }
 
     @Test
     void findByPropertyEqual() {
+        List<Ingredients> matchedIngredients = ingredientDao.findByPropertyEqual("ingredientName", "White Sugar");
+        assertEquals(1, matchedIngredients.size());
+        assertEquals(4, matchedIngredients.get(0).getIngredientID());
     }
 
     @Test
